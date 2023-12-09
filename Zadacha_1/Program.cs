6ac4fa9b52ce0f44aae2cf84152d64990d1a6bf4
Dmitry <semiashkin@tut.by>
2023-12-09 07:16:42 +0300

    адача решена

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с 
+// консоли. Программа завершается при вводе символа ‘q’ или при вводе числа,
+//  сумма цифр которого четная.
+ 
+//  Пример
+// 5 12 16 q [STOP]
+// 3 45 342 15 [STOP]
+
+using System;
+
+namespace EvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            do
+            {
+                Console.WriteLine("Введите целое число:");
+                input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                int number;
+                if (!int.TryParse(input, out number))
+                {
+                    Console.WriteLine("Неверный ввод. Введите целое число.");
+                    continue;
+                }
+
+                int sum = 0;
+                while (number > 0)
+                {
+                    sum += number % 10;
+                    number /= 10;
+                }
+
+                if (sum % 2 == 0)
+                {
+                    Console.WriteLine("Сумма цифр числа {0} четная.", input);
+                    break;
+                }
+            } while (true);
+
+            Console.WriteLine("STOP.");
+        }
+    }
+}
